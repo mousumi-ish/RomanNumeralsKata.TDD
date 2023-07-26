581db4a8c44619c2293b265406df75adef1d8f53
mousumi-ish <baral_mousumi@yahoo.com>
2023-07-26 18:48:50 +0100

    feat: written test cases for unexpected inputs and for 1,2

--- intToRoman.js
+++ intToRoman.js
@@ -1,6 +1,15 @@
-function convertToRoman() {
-  return "Number not entered";
-}
-console.log(convertToRoman());
+const convertToRoman = (num) => {
+  const index = {
+    1: "I",
+    2: "II",
+  };
 
+  if (num === undefined) return "Number is required";
+  if (num >= 5000) return "Number is greater than 5000";
+  if (num < 0) return "Romans did not require to calculate negative numbers!!";
+  if (num === 0) return "Roman did not know about 0";
+  if (num === "aaaa") return "String not allowed,Number is required";
+  if (num === 1) return "I";
+  if (num === 2) return "II";
+};
 module.exports = convertToRoman;
--- intToRoman.test.js
+++ intToRoman.test.js
@@ -1,5 +1,21 @@
 const convertToRoman = require("./intToRoman");
 
 test("no input gives an empty string", () => {
-  expect(convertToRoman()).toBe("Number not entered");
+  expect(convertToRoman()).toBe("Number is required");
 });
+test("String not allowed", () => {
+  expect(convertToRoman("aaaa")).toBe("String not allowed,Number is required");
+});
+test("0 gives an  string", () => {
+  expect(convertToRoman(0)).toBe("Roman did not know about 0");
+});
+test("will return error if the number is greater than 2999", () => {
+  expect(convertToRoman(10870)).toBe("Number is greater than 5000");
+});
+test("1 convert to I", () => {
+  expect(convertToRoman(1)).toBe("I");
+});
+test("2 convert to I", () => {
+  expect(convertToRoman(2)).toBe("II");
+});
+
